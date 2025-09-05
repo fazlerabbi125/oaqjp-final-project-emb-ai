e9121f82414caff611903a329dfb189a25321ec6
fazlerabbi125 <fazle.rabbi125faiyaz@gmail.com>
2025-09-05 01:23:29 -0400

    feat: Task3

---
+++ emotion_detection.py
@@ -0,0 +1,11 @@
+import requests, json
+
+def emotion_detector(text_to_analyze:str):
+    url: str ='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers:dict = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    payload: dict = { "raw_document": { "text": text_to_analyze } }
+    res = requests.post(url, json=payload, headers=headers)
+    res_data = json.loads(res.text)
+    emotions = res_data["emotionPredictions"][0]["emotion"]
+    emotions['dominant_emotion'] = max(emotions.items(), key=lambda x:x[1])[0]
+    return emotions
